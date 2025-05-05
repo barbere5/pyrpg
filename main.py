161c797d3025d7e96aaba74017a66974b6f10fb1
Elliot <elliotjbarb@gmail.com>
2025-05-05 17:09:56 -0400

    cleaned up funcs

--- main.py
+++ main.py
@@ -87,89 +87,135 @@ def menu_options():
 def speciesmenu():
     # Dictonary for species info 
     speciesdic = {
-        '1': {'species': 'human', 'description': 'Adaptable and well-rounded.', 'stats': { 'str' : 10, 'dex' : 10, 'int' : 10
+        '1': {'species': 'Human', 'description': 'Adaptable and well-rounded.', 'stats': { 'str' : 10, 'dex' : 10, 'int' : 10
 
         },},
-        '2': {'species': 'elf', 'description': 'Magically gifted.', 'stats': { 'str' : 7, 'dex' : 7, 'int' : 15
+        '2': {'species': 'Elf', 'description': 'Magically gifted.', 'stats': { 'str' : 7, 'dex' : 7, 'int' : 15
 
         },},
-        '3': {'species': 'dwarf', 'description': 'Gifted with a strong constitution.', 'stats': { 'str' : 15, 'dex' : 7, 'int' : 7
+        '3': {'species': 'Dwarf', 'description': 'Gifted with a strong constitution.', 'stats': { 'str' : 13, 'dex' : 7, 'int' : 10
+
+        },},
+        '4': {'species': 'Goblin', 'description': 'Fast and agile.', 'stats': { 'str' : 7, 'dex' : 15, 'int' : 7
+
+        },},
+        '5': {'species': 'Giant', 'description': '', 'stats': { 'str' : 18, 'dex' : 5, 'int' : 7
+
+        },},
+        '6': {'species': 'Skeleton', 'description': '', 'stats': { 'str' : 15, 'dex' : 10, 'int' : 4
+
+        },},
+        '7': {'species': 'Golem', 'description': 'Hewn from stone and resistant to most damage.', 'stats': { 'str' : 20, 'dex' : 3, 'int' : 4
+
+        },},
+        '8': {'species': 'Gnome', 'description': '', 'stats': { 'str' : 10, 'dex' : 10, 'int' : 10
+
+        },},
+        '9': {'species': 'Troll', 'description': '', 'stats': { 'str' : 7, 'dex' : 7, 'int' : 15
+
+        },},
+        '10': {'species': 'Orc', 'description': '', 'stats': { 'str' : 13, 'dex' : 7, 'int' : 10
+
+        },},
+        '11': {'species': 'Kobold', 'description': '', 'stats': { 'str' : 7, 'dex' : 15, 'int' : 7
+
+        },},
+        '12': {'species': 'Half-Elf', 'description': '', 'stats': { 'str' : 18, 'dex' : 5, 'int' : 7
+
+        },},
+        '13': {'species': 'Demon', 'description': '', 'stats': { 'str' : 15, 'dex' : 10, 'int' : 4
 
         },}
     }
 
-    print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
-    print("           -Character Creation-")
-    print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
-    print(' Select a species for more info:')
-    print(ANSI['fggreen']+ " 1)" + ANSI['fgreset'] + " Human")
-    print(ANSI['fggreen']+ " 2)" + ANSI['fgreset'] + " Elf")
-    print(ANSI['fggreen']+ " 3)" + ANSI['fgreset'] + " Dwarf")
-    print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
-    
+    curchoices = {}
+    used_choices = set()
+    # It works dont touch
+    while len(curchoices) < 3:
+            
+            randnum = str(random.randint(1, len(speciesdic)))
+            randsp = speciesdic[randnum]
+            spname = randsp['species']
+
+            if spname not in used_choices:
+                curchoices[str(len(curchoices) + 1)] = randsp
+                used_choices.add(spname)
+
     # Selected species
-    choice = input('-> ')
-
-    if choice not in speciesdic:
-        os.system('cls')
-        print('Not an option, Try again.')
-        speciesmenu() #recurse
-    else:
-        spacing = " " * int((41 - len(speciesdic[choice]['species'])) / 2) # spacing to center species name
-        os.system('cls')
+    def menu2():
         print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
-        print( spacing + "-" + speciesdic[choice]['species'] + "-" + spacing)
+        print("           -Character Creation-")
         print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
-        print(fspacing(speciesdic[choice]['description']) + " " + speciesdic[choice]['description'] + ' ' + fspacing(speciesdic[choice]['description']))
-
-        print(ANSI['fgred'] + " Strength: " + ANSI['fgreset'] + str(speciesdic[choice]['stats']['str']))
-        print(ANSI['fggreen'] + " Dexterity: " + ANSI['fgreset'] + str(speciesdic[choice]['stats']['dex']))
-        print(ANSI['fgblue'] + " Intelligence: " + ANSI['fgreset'] + str(speciesdic[choice]['stats']['int']))
-        print(fspacing(" Select species?") + " Select species?" + fspacing(" Select species?") )
+        print(' Select a species for more info:')
+        print(ANSI['fggreen']+ " 1)" + ANSI['fgreset'] + curchoices['1']['species'])
+        print(ANSI['fggreen']+ " 2)" + ANSI['fgreset'] + curchoices['2']['species'])
+        print(ANSI['fggreen']+ " 3)" + ANSI['fgreset'] + curchoices['3']['species'])
         print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
-        print(' 1) Yes 2) No')
+        print(fspacing("- Enter " + "[4]" + " to shuffle species -") + "- Enter " + ANSI['fgred'] + "[4]" + ANSI['fgreset'] + " to shuffle species -" + fspacing("- Enter " + "[4]" + " to shuffle species -"))
+    
 
-        # It works don't touch :o
-        confirm = input('-> ')
-        while confirm != '1' and confirm != '2':
+        choice = input('-> ')
+
+        if (choice not in curchoices) and (choice != '4'):
+            os.system('cls')
             print('Not an option, Try again.')
-            confirm = input('-> ') # needs to be fixed to solve edgecase but it works well enough rn.
-        if confirm == '1':
-            curplayer.species = speciesdic[choice]['species']
-            curplayer.str = speciesdic[choice]['stats']['str']
-            curplayer.dex = speciesdic[choice]['stats']['dex']
-            curplayer.int = speciesdic[choice]['stats']['int']
+            menu2() #recurse
+        elif choice == '4':
             os.system('cls')
-            print( speciesdic[choice]['species'] + ' Selected', end='')
-            time.sleep(0.5)
-            print('.', end='', flush=True)
-            time.sleep(0.5)
-            print('.', end='', flush=True)
-            time.sleep(0.5)
-            print('.', end='', flush=True)
-            time.sleep(0.5)
-            religionmenu()
+            speciesmenu()
+        else:
+            spacing = " " * int((41 - len(curchoices[choice]['species'])) / 2) # spacing to center species name
+            os.system('cls')
+            print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
+            print( spacing + "-" + curchoices[choice]['species'] + "-" + spacing)
+            print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
+            print(fspacing(curchoices[choice]['description']) + " " + curchoices[choice]['description'] + ' ' + fspacing(curchoices[choice]['description']))
 
-        if confirm == '2':
-            os.system('cls')
-            speciesmenu() #recurse
+            print(ANSI['fgred'] + " Strength: " + ANSI['fgreset'] + str(curchoices[choice]['stats']['str']))
+            print(ANSI['fggreen'] + " Dexterity: " + ANSI['fgreset'] + str(curchoices[choice]['stats']['dex']))
+            print(ANSI['fgblue'] + " Intelligence: " + ANSI['fgreset'] + str(curchoices[choice]['stats']['int']))
+            print(fspacing(" Select species?") + " Select species?" + fspacing(" Select species?") )
+            print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
+            print(' 1) Yes 2) No')
+
+            # It works don't touch :o
+            confirm = input('-> ')
+            while confirm != '1' and confirm != '2':
+                print('Not an option, Try again.')
+                confirm = input('-> ') # needs to be fixed to solve edgecase but it works well enough rn.
+            if confirm == '1':
+                curplayer.species = curchoices[choice]['species']
+                curplayer.str = curchoices[choice]['stats']['str']
+                curplayer.dex = curchoices[choice]['stats']['dex']
+                curplayer.int = curchoices[choice]['stats']['int']
+                os.system('cls')
+                print( curchoices[choice]['species'] + ' Selected', end='')
+                time.sleep(0.5)
+                print('.', end='', flush=True)
+                time.sleep(0.5)
+                print('.', end='', flush=True)
+                time.sleep(0.5)
+                print('.', end='', flush=True)
+                time.sleep(0.5)
+                religionmenu()
+
+            if confirm == '2':
+                os.system('cls')
+                menu2() #recurse
+    menu2()
 
 
 def religionmenu():
         
-        descript = ['Nameless', 'Infernal', 'Stygian', 'Abyssal', 'Adorable', 'Aggressive', 'Formless', 'Alert', 'Alive', 'Amused', 'Angry',
- 'Annoyed', 'Annoying', 'Anxious', 'Arrogant', 'Ashamed', 'Rotting', 'Awful', 'Blind', 'Black', 'Bloody', 'Blue', 'Blue-Eyed', 'Blushing',
- 'Bored', 'Brave', 'Bright', 'Busy', 'Calm', 'Careful', 'Cautious',
- 'Charming', 'Cheerful', 'Corrupt', 'Clever', 'Cloudy', 'Clumsy', 'Colorful', 'Combative',
- 'Comfortable', 'Concerned', 'Condemned', 'Confused', 'Bloody', 'Courageous',
- 'Dead', 'Cruel', 'Curious', 'Dangerous', 'Dark', 'Dead', 'Defeated',
- 'Defiant', 'Delightful', 'Depressed', 'Determined', 'Different', 'Difficult', 'Disgusted',
- 'Bloody', 'Disturbed', 'Dizzy', 'Doubtful', 'Dull', 'Baleful', 'Cursed', 'Skinless', 'Barbed', 'Ominous', 'Blood-Black']
-
+        descript = ['Fell', 'Alive', 'Writhing', 'Disturbed', 'Dead', 'Defiant', 'Festering', 'Barbed',
+ 'Corrupted', 'Cruel', 'Putrid', 'Dark', 'Maddening', 'Colorful', 'Skinless',
+ 'Cursed', 'Bleeding', 'Nameless', 'Rotting', 'Black', 'Baleful', 'Abyssal',
+ 'Dead', 'Molten', 'Blighted', 'Stygian', 'Blood-Black', 'Blind', 'Unseen',
+ 'Formless', 'Bright', 'Ominous', 'Infernal', 'Bloody', 'Hollow', 'Mad']
         
         
         
-        noun = ['Egg', 'Beast', 'Star', 'Sun', 'Moon', 'Mountain', 'Storm', 'Planet']
+        noun = ['Monolith', 'Obelisk', 'Beast', 'Star', 'Sun', 'Moon', 'Mountain', 'Storm', 'Planet', 'Serpent', 'Radiance', 'Hunger', 'Wyrm', 'Leviathan', 'Eye']
         shrine_features = ['Barbed', 'Ominous', '']
         shrine_materials = ['Copper', 'Wood', 'Iron', 'Black', 'White', 'Marble', 'Stone', 'Blue', 'Azure', 'Glass', 'Dirt']
         shrine_forms = ['Obelisk', 'Menhir', 'Monolith', 'Dolmen']
@@ -194,7 +240,7 @@ def religionmenu():
             print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
             print('  ' + ANSI["fgreset"] + "- Enter " + ANSI['fgred'] + "[4]" + ANSI['fgreset'] + " to generate new religions -")
 
-            # There is a better way to do this that is not repetitive dexaghetti but I am losing my mind and it works.
+            # There is a better way to do this that is not repetitive spaghetti but I am losing my mind and it works.
             confirm = input('-> ')
             while confirm != '1' and confirm != '2' and confirm != '3' and confirm != '4':
                 print('Not an option, Try again.')
@@ -305,7 +351,7 @@ def charname():
         confirm = input('-> ') 
     if confirm == '1':
         os.system('cls')
-        curplayer.name = name
+        curplayer.name = name.title()
         print(name + ' Selected', end='')
         time.sleep(0.5)
         print('.', end='', flush=True)
@@ -328,7 +374,7 @@ def gamestart():
     print("               -Character-")
     print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
     print(" The " + ANSI['fggreen'] + curplayer.species + ANSI['fgreset'] + " " + curplayer.name)
-    print(" follower of " + ANSI['fgblue'] + curplayer.religion + ANSI['fgreset']+ '.')
+    print(" Follower of " + ANSI['fgblue'] + curplayer.religion + ANSI['fgreset']+ '.')
     print(ANSI['fgbrown'] + " +---------------------------------------+" + ANSI['fgreset'])
     print(" Confirm Character?")
     print(' 1) Yes 2) No')
@@ -346,7 +392,7 @@ def gamestart():
         time.sleep(0.5)
         print('.', end='', flush=True)
         time.sleep(0.5)
-        wandering()
+        gameloop()
     if confirm == '2':
         os.system('cls')
         print('Returning to Character creation menu', end='')
@@ -361,53 +407,13 @@ def gamestart():
         speciesmenu()
 
 
+def gameloop():
+    pass
+
 def wandering():
     os.system('cls')
     world()
     visualizemap()
-
-
-# cool func but not using rn.
-def worldgen():
-    print('How big do you want your world to be?')
-    print('1) Small[8x8] 2) Medium[16x16] 3) Large[32x32]')
-    choice = int(input('-> '))
-    if choice > 3 or choice < 1:
-        os.system('cls')
-        print("Input not an option.")
-        worldgen()
-    else:
-        size = 8 * choice
-    
-    # generate a list of sublists 
-    arr = []
-    subarr = []
-    for i in range(size):
-        for  n in range(size):
-            subarr.append(random.randint(0,3))
-        arr.append(subarr)
-        subarr = []
-    #1print(arr)
-
-
-    hill = ANSI['fgbrown']+ '∩' + ANSI['fgreset']
-    mountain = ANSI['fggrey'] + '▲' + ANSI['fgreset']
-    water = ANSI['fgblue']+ '≈' + ANSI['fgreset']
-    plains = ANSI['fggreen']+ '≡' + ANSI['fgreset']
-    town =  ANSI['fgbrown']+ '♦' + ANSI['fgreset']
-
-    mapkey = [hill, mountain, plains, water]
-    for i in arr:
-        #print(i)
-        mapstr = ''
-        for n in i:
-            #print(n)
-            mapstr += mapkey[n]
-        print(mapstr)
-    print("World key: water= hill= mountain= plains= ln/.;lm/")
-    print("Keep world?")
-    input('-> ')
-
 
 def world():
     # 8X8 matrix world map base
@@ -418,7 +424,6 @@ def world():
             x.append(str(random.randint(0,3)))
         matrix.append(x)
     return matrix
-    # Turn matrix into str map 
 
 def visualizemap():
     hill = ANSI['fgbrown']+ '∩' + ANSI['fgreset']
@@ -437,7 +442,9 @@ def visualizemap():
     print(ANSI['fgbrown'] + "+-------------------------------+" + ANSI['fgreset'])
     print(' Map Key: Hills = {} Mountains = {} \n Plains = {} Water = {}'.format(mapkey[0], mapkey[1], mapkey[2], mapkey[3]))
     print(ANSI['fgbrown'] + "+-------------------------------+" + ANSI['fgreset'])
-visualizemap()
+
+
+
 
 # call funcs
 menu()
